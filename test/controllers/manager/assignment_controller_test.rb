a67cf6e858f10c99f87b65a8d037ea16cfd11d22
IROHA-123 <192299303+IROHA-123@users.noreply.github.com>
2025-06-10 22:13:10 +0900

    Remove unused manager assignment test

--- test/controllers/manager/assignment_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class Manager::AssignmentControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get manager_assignment_index_url
-    assert_response :success
-  end
-end
